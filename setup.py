be64f935dfdd1e9c372847fab8c2a7caaf70fee9
dellumdeus <alion.develop@gmail.com>
2018-12-22 02:40:12 +0100

    Update setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='donbeer',
+    version='1.0.0',
+    packages=['project/oop/v2', ],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)

2f90ae9aeaa1ed7f52f11ccab633e987c4ff6de2
dellumdeus <alion.develop@gmail.com>
2021-12-03 20:50:34 -0500

    Add more tests

--- donbeer/tests/test_game.py
+++ donbeer/tests/test_game.py
@@ -1,6 +1,8 @@
 from donbeer.game import Game
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 import pytest
+import pdb
+import time
 
 beer = MagicMock()
 donut = MagicMock()
@@ -16,7 +18,7 @@ def test_new_round(game):
     game.new_round(beer)
 
     assert game.round == old_game_round + 1
-    assert game.ran_num and game.random_time != None
+    assert game.ran_num and game.random_time is not None
     assert beer.clicks == 0
 
 
@@ -24,7 +26,7 @@ def test_get_status_when_waiting_starts(game):
     beer.clicks = game.ran_num
     status = game.get_status(beer)
 
-    assert game.donut_wait_start != None
+    assert game.donut_wait_start is not None
     assert status == 'donut'
 
 
@@ -38,3 +40,31 @@ def test_get_status_when_waiting(game):
 def test_get_status_when_waiting_is_over(game):
     status = game.get_status(beer)
     assert status == 'beer'
+
+
+@pytest.fixture(autouse=True)
+def mock_texts():
+    mock_texts = [MagicMock() for n in range(3)]
+    for idx, item in enumerate(mock_texts):
+        item.name = f"text {idx}"
+    return mock_texts
+
+
+def test_get_text_when_it_matches(game, mock_texts):
+    game.texts = mock_texts
+    assert game.get_text("text 1") == mock_texts[1]
+
+
+def test_get_text_when_it_matches_not(game, mock_texts):
+    game.texts = mock_texts
+    assert game.get_text("text 1") != mock_texts[2]
+
+
+@patch('donbeer.game.sleep', return_value=None)
+def test_countdown_while_running(patched_sleep, game):
+    game.game_time = 60
+    game.is_finished = False
+    game.countdown()
+    assert patched_sleep.call_count == 61
+    assert game.game_time == -1
+    assert game.is_finished
